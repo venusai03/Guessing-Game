1ec3efe8a7211c0701eaef11c935c453e45459e5
Venu Sai Madisetti <venu@betacrew.io>
2023-10-02 15:46:31 +0530

    add guessing game

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
+import "./App.css"
+import {useRef, useState} from "react";
+
+const GUESS_THE_NUMBER_STRING = "Guess the target number";
+const LESS_THAN_STRING = "The number is less";
+const GREATER_THAN_STRING = "The number is greater";
+const CORRECT_GUESS_STRING = "Yay! You guessed the number!";
+const LOSER_STRING = "You lost!";
+const ATTEMPTS_REMAINING_STRING = "Attempts remaining: ";
+
 function App() {
+    const [status, setStatus] = useState(GUESS_THE_NUMBER_STRING);
+    const [attempts, setAttempts] = useState(10);
+
+    const numberToBeGuessed = useRef(guessInteger());
+
+    function handleSubmit(guessedNumber) {
+        if (attempts === 0 || status === CORRECT_GUESS_STRING) {
+            return;
+        }
+
+        if (guessedNumber === numberToBeGuessed.current) {
+            setStatus(CORRECT_GUESS_STRING);
+        } else if (guessedNumber < numberToBeGuessed.current) {
+            setStatus(LESS_THAN_STRING);
+        } else {
+            setStatus(GREATER_THAN_STRING);
+        }
+
+        if (attempts === 1) {
+            setStatus(LOSER_STRING + " The number is " + numberToBeGuessed.current);
+        }
+
+        setAttempts(attempts - 1);
+    }
+
+    function handleReset() {
+        numberToBeGuessed.current = guessInteger();
+        setAttempts(10);
+        setStatus(GUESS_THE_NUMBER_STRING);
+    }
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <p>{status}</p>
+            <p>{ATTEMPTS_REMAINING_STRING + attempts}</p>
+            <input type="number" id="guessedNumberField"/>
+            <div className="buttonsDiv">
+                <button className="buttons"
+                        onClick={() => handleSubmit(parseInt(document.getElementById("guessedNumberField").value))}>Submit
+                </button>
+                <button className="buttons" onClick={handleReset}>Reset</button>
+            </div>
+        </>
     );
+}
+
+function guessInteger(maxLimit = 101) {
+    let randomNumber = Math.random() * maxLimit;
+    return Math.floor(randomNumber);
 }
 
 export default App;
